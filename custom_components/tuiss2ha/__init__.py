178f1a603cd40fa01cb4b6247885fad12c04f3bf
pink88 <nigel.pink@gmail.com>
2023-03-20 22:27:36 +0000

    renamed int folder

---
+++ custom_components/tuiss2ha/__init__.py
@@ -0,0 +1,31 @@
+"""The Detailed Hello World Push integration."""
+from __future__ import annotations
+
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from . import hub
+from .const import DOMAIN
+
+
+
+PLATFORMS: list[str] = ["cover"]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Hello World from a config entry."""
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = hub.Hub(hass, entry.data["host"], entry.data["name"])
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
---
+++ custom_components/tuiss2ha/config_flow.py
@@ -0,0 +1,71 @@
+"""Config flow for Hello World integration."""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant import config_entries, exceptions
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN  # pylint:disable=unused-import
+from .hub import Hub
+
+_LOGGER = logging.getLogger(__name__)
+
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required("host", default="XX:XX:XX:XX:XX:XX"): str,
+        vol.Required("name", default="Name for device"): str
+    }
+)
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Hello World."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_ASSUMED
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            try:
+                _title = await validate_input(self.hass, user_input)
+
+                return self.async_create_entry(title=_title, data=user_input)
+            except CannotConnect:
+                errors["name"] = "Your name must be longer than 0 characters"
+            except InvalidHost:
+                errors["host"] = "Your host should be a valid MAC address in the format XX:XX:XX:XX:XX:XX"
+            except Exception:  
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
+
+
+async def validate_input(hass: HomeAssistant, data: dict) -> dict[str, Any]:
+    """Validate the user input allows us to connect."""
+    
+    if len(data["host"]) < 17:
+        raise InvalidHost
+
+    if len(data["name"]) == 0 :
+        raise InvalidName
+
+    return data["name"]
+
+
+
+
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidHost(exceptions.HomeAssistantError):
+    """Error to indicate there is an invalid hostname."""
